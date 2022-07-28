4c85a0bdfe9bae50192f2491d3ef4c7dd351bf13
teityou <510350964@qq.com>
2022-07-28 20:53:22 +0800

    updata

--- src/views/权限管理/角色列表/index.vue
+++ src/views/权限管理/角色列表/index.vue
@@ -6,30 +6,32 @@
       <el-table border :data="Roleslist" row-key="id">
         <el-table-column label="#" type="index" />
         <el-table-column label="角色名称" prop="roleName" />
-
         <el-table-column label="角色描述" prop="roleDesc" />
         <el-table-column label="操作">
-          <el-button type="primary" size="mini" icon="el-icon-edit"
-            >_编辑</el-button
-          >
-          <el-button type="danger" size="mini" icon="el-icon-delete"
-            >_删除</el-button
-          >
-          <el-button type="warning" size="mini" icon="el-icon-setting"
-            @click= "showDialog=true">权限分配</el-button
-          >
+          <el-button type="primary" size="mini" icon="el-icon-edit">_编辑</el-button>
+          <el-button type="danger" size="mini" icon="el-icon-delete">_删除</el-button>
+          <el-button
+            type="warning"
+            size="mini"
+            icon="el-icon-setting"
+            @click="getPowerlist">权限分配</el-button>
         </el-table-column>
       </el-table>
     </el-card>
-    <el-dialog>
+    <el-dialog title="权限分配" :visible.sync="showDialogVisible" width="80%">
       <el-tree
         :data="data"
         show-checkbox
         node-key="id"
         :default-expanded-keys="[2, 3]"
         :default-checked-keys="[5]"
-        :props="defaultProps"
+        :props="treeProps"
+        default-expand-all
       />
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="showDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="showDialogVisible = false">确 定</el-button>
+      </span>
     </el-dialog>
   </div>
 </template>
@@ -44,14 +46,17 @@ export default {
     return {
       Roleslist: [],
       data: [],
-      showDialog: false
+      showDialogVisible: false,
+      treeProps: {
+        label: 'authName',
+        children: 'children'
+      }
     }
   },
   computed: {},
   watch: {},
   created () {
     this.getRoleslist()
-    this.getPowerlist('tree')
   },
   methods: {
     async getRoleslist () {
@@ -59,10 +64,12 @@ export default {
       console.log(res)
       this.Roleslist = res
     },
+
     async getPowerlist (type) {
-      const res = await getPowerlist(type)
+      const res = await getPowerlist('tree')
       console.log(res)
       this.data = res
+      this.showDialogVisible = true
     }
 
   }
